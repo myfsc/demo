f7490699186c3e8389401d3250f1dc5c4eacf48c
myfsc <140025741+myfsc@users.noreply.github.com>
2023-07-31 00:30:38 -0400

    one space

--- code00.sql
+++ code00.sql
@@ -4,5 +4,5 @@ select "users: "||count(*) from dba_users;
 select sysdate from dual;
 REM this is a comment from branch01
 REM this is a comment from branch02
-select select count(*) tablespaces from dba_tablespaces;
+select select count(*) tablespaces from dba_tablespaces ;
 REM comment after tablespace query
