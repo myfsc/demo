0fc8223249a5cb77c513f7111aebf159d8469006
myfsc <140025741+myfsc@users.noreply.github.com>
2023-07-30 23:09:40 -0400

    commit 2

--- code00.sql
+++ code00.sql
@@ -2,8 +2,6 @@ select * from dual;
 Select "Hi, this is a test" from dual;
 select "users: "||count(*) from dba_users;
 select sysdate from dual;
-
 REM This is a comment from brach01
-
-REM this is a comment
+REM this is a comment 
 
